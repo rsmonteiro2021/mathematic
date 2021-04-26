a5d3bbf4243be79118ed93d7701e445ea40d1254
Roberto Monteiro <monteiroengenharia@gmail.com>
2021-04-26 00:40:35 -0300

    criei o módulo right_triangle

--- right_triangle.py
+++ right_triangle.py
@@ -11,15 +11,3 @@ def hipotenusa(base, altura):
     import math
     a = math.sqrt(pow(base,2) + pow(altura,2))
     return a
-
-print('\nCalcula a área e a hipotenusa de um triângulo retângulo!')
-base = int(input('Digite a base do triângulo retângulo:\nPara encerrar digite 0!\n'))
-while base > 0:
-    if base == 0:
-        print('Programa encerrado com sucesso!')
-        break
-    altura = int(input('Digite a altura do triângulo retângulo:\n'))
-    h = hipotenusa(base, altura)
-    area_triangulo = area_triangulo_retangulo(base, altura)
-    print(f'\nResposta:\n\tO triângulo retângulo de lados {base} e {altura} possui hipotenusa = {h} e área = {area_triangulo}\n')
-    base = int(input('Digite a base do novo triângulo retângulo:\nPara encerrar digite 0!\n'))
